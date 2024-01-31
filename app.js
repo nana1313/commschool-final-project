65b20e395ea51ec9de1b1f6800bdeba1675af36f
nana dumbadze <55438500+nana1313@users.noreply.github.com>
2024-01-31 22:59:46 +0400

    add slide

--- app.js
+++ app.js
@@ -19,26 +19,15 @@ function showSlides(n) {
 }
 
 //მოდალი
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+let btn = document.getElementById("explore-more");
+let span = document.getElementsByClassName("close")[0];
+btn.addEventListener("click",function() {
+    let modal = document.getElementById("myModal");
+    modal.style.display = "block";
+  });
+span.addEventListener("click",function() {
   modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
+});
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
